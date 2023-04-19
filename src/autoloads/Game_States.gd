f91b30c2ad53850f5cc152ccf62d72d8ee540907
Jonathan Gonzalez <jgonzalez249@gmail.com>
2023-04-19 14:58:22 -0400

    level_3 dialogue complete

--- src/autoloads/Game_States.gd
+++ src/autoloads/Game_States.gd
@@ -3,6 +3,11 @@ extends Node2D
 
 var level: int = 0
 
-# For Level_1 Dialogue
+# For Dialogue
 var has_met_AI:bool = false
 var has_met_AI_doubleJump:bool =  false
+
+var block_level4:bool=true
+
+var die:bool=false
+var ending:bool = false
--- src/scripts/loseGloves.gd
+++ src/scripts/loseGloves.gd
@@ -3,7 +3,6 @@ extends Area2D
 signal loseGloves
 
 
-func _on_loseGloves_body_entered(body, _extra_arg_0):
-	print("lost gloves")
+func _on_loseGloves_body_entered(_body, _extra_arg_0):
 	emit_signal("loseGloves")
 	self.queue_free()
